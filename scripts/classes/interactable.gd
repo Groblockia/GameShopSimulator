71506b5feace4cdb4bf8f96a490ed774c87980b8
Groblockia <groblovroum@gmail.com>
2026-03-14 14:01:56 +0100

    test

--- scripts/classes/interactable.gd
+++ scripts/classes/interactable.gd
@@ -7,6 +7,7 @@ class_name Interactable extends Area3D
 
 func interaction(_player: Player, _charged_time: float) -> void:
 	print("don't forget to override interaction()")
+	print("test")
 
 func start_highlight(_player: Player, _charged_time: float) -> void:
 	print("don't forget to override start_highlight()")
